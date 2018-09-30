ad01fbd12782666e8a24c57666c8fbae995dc3da
Miguel Baptista <miguelarhb@gmail.com>
2018-09-30 15:51:51 +0100

    yup

--- user.py
+++ user.py
@@ -5,7 +5,7 @@ import socket
 TCP_IP = socket.gethostbyname("tejo.tecnico.ulisboa.pt")
 TCP_PORT = 58011
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = "login 67890 aaaaaaaa"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
